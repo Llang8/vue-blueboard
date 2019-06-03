6631b9911c92af61d9afa2ef1ea36bf6dbfde203
Lucas Lang <lucaslang8@gmail.com>
2019-06-02 23:56:26 -0500

    Delete config.py

--- backend/Flask/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this'
-
-    SQLALCHEMY_DATABASE_URI = 'postgres://Llang8:Tamemonkey12!@blueboard.cgsf4ufm4fjo.us-east-2.rds.amazonaws.com:5432/blueboard'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
